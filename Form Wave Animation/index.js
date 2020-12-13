58891261eff0076601b034a1f9769fc34a47672d
AK <balaajay19@gmail.com>
2020-12-13 15:52:10 +0530

    Form Wave Animation

---
+++ Form Wave Animation/index.js
@@ -0,0 +1,13 @@
+const labels = document.querySelectorAll(".form-control label");
+
+console.log(labels);
+
+labels.forEach((label) => {
+  label.innerHTML = label.innerText
+    .split("")
+    .map(
+      (letter, index) =>
+        `<span style="transition-delay:${index * 50}ms">${letter}</span>`
+    )
+    .join("");
+});
