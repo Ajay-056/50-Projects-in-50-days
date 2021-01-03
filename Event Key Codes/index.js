5363d6c3fbb27f969eadea6bccb2c289bf0d71a4
AK <balaajay19@gmail.com>
2021-01-03 11:01:23 +0530

    Added Event Key Codes

---
+++ Event Key Codes/index.js
@@ -0,0 +1,12 @@
+const insert = document.getElementById("insert");
+const key = document.getElementById("key");
+const keyCode = document.getElementById("keyCode");
+const code = document.getElementById("code");
+
+window.addEventListener("keydown", (e) => {
+  insert.innerHTML = `<div class="key">${
+    e.key === " " ? "Space" : e.key
+  }<small>event.key</small></div>
+  <div class="key">${e.keyCode}<small>event.keyCode</small></div>
+  <div class="key">${e.code}<small>event.code</small></div>`;
+});
